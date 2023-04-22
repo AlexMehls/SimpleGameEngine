635bf8eb722ec819db11f845cf20533152e9bf34
Alexander Mehls <alexmehls@t-online.de>
2023-04-22 22:57:10 +0200

    Cleaned up code

--- main.cpp
+++ main.cpp
@@ -9,6 +9,31 @@
 #include <iostream>
 #include <chrono>
 
+void cameraMoveDemo(Camera &camera, double deltaTime)
+{
+    static int cameraMoveDirection = 1;
+    const float cameraMoveSpeed = 1;
+
+    camera.move(glm::vec3(deltaTime * cameraMoveSpeed * cameraMoveDirection, 0, 0));
+    if (camera.getPos().x >= 5)
+    {
+        cameraMoveDirection = -1;
+    }
+    else if (camera.getPos().x <= -5)
+    {
+        cameraMoveDirection = 1;
+    }
+    camera.lookAt(glm::vec3(0));
+}
+
+void cameraMouseControl(Camera &camera, UserInput &input)
+{
+    const float mouseSpeed = 0.0005f;
+
+    camera.rotate(mouseSpeed * input.mouseDeltaX,
+                  mouseSpeed * input.mouseDeltaY);
+}
+
 int main(int argc, char *argv[])
 {
     GameEngine &engine = GameEngine::getInstance();
@@ -25,11 +50,6 @@ int main(int argc, char *argv[])
     Camera camera(engine.vertexbuffer, engine.colorbuffer, engine.mvpMatrixId);
     camera.setPos(glm::vec3(0, 2, 5));
     camera.lookAt(glm::vec3(0));
-    float speed = 1;
-    float mouseSpeed = 0.0005f;
-
-    int cameraMoveDirection = 1;
-    float cameraMoveSpeed = 1;
 
     float ratio;
     int width, height;
@@ -38,7 +58,8 @@ int main(int argc, char *argv[])
     double prevTime = glfwGetTime();
     double curTime;
     double deltaTime;
-    double fps;
+    double realFps;
+    double gameFps;
 
     while (!glfwWindowShouldClose(engine.window))
     {
@@ -46,32 +67,28 @@ int main(int argc, char *argv[])
         deltaTime = curTime - prevTime;
         prevTime = curTime;
 
-        fps = 1 / deltaTime;
-        // std::cout << fps << std::endl;
+        realFps = 1 / deltaTime;
+
+        if (deltaTime > (double)1 / 30)
+        {
+            deltaTime = 1 / 30;
+        }
+
+        gameFps = 1 / deltaTime;
+        // std::cout << realFps << std::endl;
 
         glfwGetFramebufferSize(engine.window, &width, &height);
         ratio = width / (float)height;
-        // Setting a fixed resolution causes stretching if window shape changes
-        camera.setRatio(ratio);
-
-        camera.rotate(mouseSpeed * input.mouseDeltaX,
-                      mouseSpeed * input.mouseDeltaY);
-
-        /*
-        camera.move(glm::vec3(deltaTime * cameraMoveSpeed * cameraMoveDirection, 0, 0));
-        if (camera.getPos().x >= 5)
-        {
-            cameraMoveDirection = -1;
-        }
-        else if (camera.getPos().x <= -5)
-        {
-            cameraMoveDirection = 1;
-        }
-        camera.lookAt(glm::vec3(0));
-        */
 
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Setting a fixed resolution causes stretching if window shape changes
+        // Updating causes lags and crashes when switching to desktop
+        // camera.setRatio(ratio);
+
+        cameraMouseControl(camera, input);
+        // cameraMoveDemo(camera, deltaTime);
 
         camera.draw(cube);
         camera.draw(triangle);
