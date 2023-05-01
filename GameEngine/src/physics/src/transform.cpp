e42f93be08ba9c393123945a8108b5c78322ab5c
Alexander Mehls <alexmehls@t-online.de>
2023-05-01 16:37:51 +0200

    Added missing Transform getters and setters

--- GameEngine/src/physics/src/transform.cpp
+++ GameEngine/src/physics/src/transform.cpp
@@ -11,44 +11,140 @@ Transform::Transform()
     return;
 }
 
+// GLOBAL GETTERS
+
+// Returns position in world-space in x, y, z
+const glm::vec3 &Transform::getPos() const
+{
+    if (parent == nullptr)
+    {
+        return position;
+    }
+    return position + parent->getPos();
+}
+// Returns rotation in world-space as quaternion
+const glm::quat &Transform::getRot() const
+{
+    if (parent == nullptr)
+    {
+        return rotation;
+    }
+    return rotation * parent->getRot();
+}
+// Returns rotation in world-space in pitch, yaw, roll
+glm::vec3 Transform::getEulerAngles() const
+{
+    return glm::eulerAngles(getRot());
+}
+// Returns scale in world-space in x, y, z
+const glm::vec3 &Transform::getScale() const
+{
+    if (parent == nullptr)
+    {
+        return scale;
+    }
+    return scale * parent->getScale();
+}
+
+// LOCAL GETTERS
+
+// Returns position in local space in x, y, z
 const glm::vec3 &Transform::getLocalPos() const
 {
     return position;
 }
+// Returns rotation in local space as quaternion
 const glm::quat &Transform::getLocalRot() const
 {
     return rotation;
 }
+// Returns rotation in local space in pitch, yaw, roll
 glm::vec3 Transform::getLocalEulerAngles() const
 {
-    // TODO: convert quaternion to angles
-    return glm::vec3(0);
+    return glm::eulerAngles(rotation);
 }
+// Returns scale in local space in x, y, z
 const glm::vec3 &Transform::getLocalScale() const
 {
     return scale;
 }
 
+// GLOBAL SETTERS
+
+// Sets position in world-space in x, y, z
+void Transform::setPos(const glm::vec3 &pos)
+{
+    if (parent == nullptr)
+    {
+        position = pos;
+    }
+    else
+    {
+        position = pos - parent->getPos();
+    }
+    return;
+}
+// Sets rotation in world-space as quaternion
+void Transform::setRot(const glm::quat &rot)
+{
+    if (parent == nullptr)
+    {
+        rotation = rot;
+    }
+    else
+    {
+        rotation = rot * glm::conjugate(parent->getRot());
+    }
+    return;
+}
+// Sets rotation in world-space in pitch, yaw, roll
+void Transform::setEulerAngles(const glm::vec3 &angles)
+{
+    setRot(glm::quat(angles));
+    return;
+}
+// Sets scale in world-space in x, y, z
+void Transform::setScale(const glm::vec3 &scale)
+{
+    if (parent == nullptr)
+    {
+        Transform::scale = scale;
+    }
+    else
+    {
+        Transform::position = scale / parent->getScale();
+    }
+    return;
+}
+
+// LOCAL SETTERS
+
+// Sets position in local space in x, y, z
 void Transform::setLocalPos(const glm::vec3 &pos)
 {
     position = pos;
     return;
 }
+// Sets rotation in local space as quaternion
 void Transform::setLocalRot(const glm::quat &rot)
 {
     rotation = rot;
     return;
 }
+// Sets rotation in local space in pitch, yaw, roll
 void Transform::setLocalEulerAngles(const glm::vec3 &angles)
 {
-    // TODO: convert angles to quat
+    rotation = glm::quat(angles);
     return;
 }
+// Sets scale in local space in x, y, z
 void Transform::setLocalScale(const glm::vec3 &scale)
 {
     Transform::scale = scale;
+    return;
 }
 
+// Updates the previous values with the current values
 void Transform::savePreviousState()
 {
     prev_position = position;
